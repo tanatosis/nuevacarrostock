46fe28c293bfa58c04ab39e821a00077a84c5f14
bryanjv <bryan.jeldes@gmail.com>
2023-02-17 10:14:17 -0300

    infinito commit

--- nuevacarrostock/js/admin.js
+++ nuevacarrostock/js/admin.js
@@ -1,7 +1,6 @@
 const cargarProductos = async() => {
 
     try {
-
         const respuesta = await fetch('https://bsite.net/metalflap/td-producto/idSucursal/9');
         console.log(respuesta);
 
@@ -13,15 +12,15 @@ const cargarProductos = async() => {
 
             productos +=
                 `
-                     <img class="producto-imagen" src="${producto.link}">
+                    <img class="producto-imagen" src="${producto.link}">
                     <div class="producto-detalles">
                         <h3 class="producto-titulo">${producto.nombre}</h3>
                         <p class="producto-precio">$${producto.precio}</p>
                         <p>Stock: ${producto.stock}</p>
                     </div>
 
-                    <button class="producto-eliminar" ">eliminar</button>
-                    <button class="producto-modificar" ">modificar</button>
+                    <button class="producto-eliminar" id="eliminarProd" onclick="eliminarProducto(${producto.id})">eliminar</button>
+                    <button class="producto-modificar" id="modificarProd" onclick="modificar(${producto.id})">modificar</button>
                 `;
 
         });
@@ -30,8 +29,45 @@ const cargarProductos = async() => {
     } catch (error) {
         console.log(error);
     }
+} 
 
+const eliminarProducto = async(id) => {
+    
+    try{
+            await fetch(`https://bsite.net/metalflap/td-producto/${id}`,
+            {method: 'DELETE', 
+            headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+            })
+    } catch (error) {
+        console.log(error);
+    }
+}
 
+const modificarProducto = async(id) => {
+        let producto = {
+            id: id,
+            nombre: ,
+            precio: ,
+            link: ,
+            stock: ,
+            etiqueta: ,
+            descripcion: "-",
+            idCategoria: ,
+            idSucursal: 9,
+        }
+
+        console.log(JSON.stringify(producto));
+
+        await fetch('https://bsite.net/metalflap/td-producto',
+        {method: 'PUT', 
+        body: JSON.stringify(producto),
+        headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+        })
+}
+
+modificar(id){
+    window.location.href("../modificarproducto.html")
+    var 
 }
 
 cargarProductos();
